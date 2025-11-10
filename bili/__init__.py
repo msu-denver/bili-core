ca0a8e927ce1f448622fb64c17e4e30b3a8a6108
Kade <kswork9@gmail.com>
2025-11-10 15:35:57 -0700

    added graph builder to init

--- bili/__init__.py
+++ bili/__init__.py
@@ -1,11 +1,13 @@
-from . import auth, checkpointers, config, flask_api, loaders, tools, utils
+from . import auth, checkpointers, config, flask_api, graph_builder, loaders, nodes, tools, utils
 
 __all__ = [
     "auth",
     "checkpointers",
     "config",
     "flask_api",
+    "graph_builder",
     "loaders",
+    "nodes",
     "tools",
     "utils",
 ]
